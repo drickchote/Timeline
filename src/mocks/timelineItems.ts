e905aca187bf84ae6e96dc347aa0d2301d4904d9
Marcos Adriano <mdrick19@gmail.com>
2025-08-08 15:42:27 -0300

    chore: add timeline mock

---
+++ src/mocks/timelineItems.ts
@@ -0,0 +1,116 @@
+export const timelineItems = [
+  {
+    id: 1,
+    start: "2021-01-14",
+    end: "2021-01-22",
+    name: "Recruit translators",
+    color: "#8B5CF6", // purple
+  },
+  {
+    id: 2,
+    start: "2021-01-17",
+    end: "2021-01-31",
+    name: "Create lesson plan 1",
+    color: "#10B981", // green
+  },
+  {
+    id: 3,
+    start: "2021-02-05",
+    end: "2021-02-13",
+    name: "Translate phrases for lesson 1",
+    color: "#F59E0B", // amber
+  },
+  {
+    id: 4,
+    start: "2021-02-07",
+    end: "2021-03-08",
+    name: "Create dark mode design",
+    color: "#EF4444", // red
+  },
+  {
+    id: 5,
+    start: "2021-02-14",
+    end: "2021-02-22",
+    name: "Recruit copyeditors",
+    color: "#3B82F6", // blue
+  },
+  {
+    id: 6,
+    start: "2021-02-18",
+    end: "2021-02-24",
+    name: "Proofread translations for lesson 1",
+    color: "#8B5CF6", // purple
+  },
+  {
+    id: 7,
+    start: "2021-02-20",
+    end: "2021-02-22",
+    name: "Finalize logo",
+    color: "#10B981", // green
+  },
+  {
+    id: 8,
+    start: "2021-02-21",
+    end: "2021-03-22",
+    name: "Implement dark mode",
+    color: "#F59E0B", // amber
+  },
+  {
+    id: 9,
+    start: "2021-02-21",
+    end: "2021-02-28",
+    name: "Finalize lesson plan 1",
+    color: "#EF4444", // red
+  },
+  {
+    id: 10,
+    start: "2021-02-23",
+    end: "2021-02-23",
+    name: "Approve logo",
+    color: "#3B82F6", // blue
+  },
+  {
+    id: 11,
+    start: "2021-03-03",
+    end: "2021-03-29",
+    name: "Create lesson plan 2",
+    color: "#8B5CF6", // purple
+  },
+  {
+    id: 12,
+    start: "2021-03-30",
+    end: "2021-04-08",
+    name: "Translate phrases for lesson 2",
+    color: "#10B981", // green
+  },
+  {
+    id: 13,
+    start: "2021-04-01",
+    end: "2021-04-04",
+    name: "Debug mobile notification error",
+    color: "#F59E0B", // amber
+  },
+  {
+    id: 14,
+    start: "2021-04-05",
+    end: "2021-04-06",
+    name: "Test debugged mobile notifications",
+    color: "#EF4444", // red
+  },
+  {
+    id: 15,
+    start: "2021-04-16",
+    end: "2021-04-30",
+    name: "Beta test",
+    color: "#3B82F6", // blue
+  },
+  {
+    id: 16,
+    start: "2021-05-01",
+    end: "2021-05-01",
+    name: "Launch day",
+    color: "#8B5CF6", // purple
+  },
+];
+
+export default timelineItems;
